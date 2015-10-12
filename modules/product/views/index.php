b1b8f80360a67d130ce8b2c9fea229508bc700cf
iddan <iddan_gunawan@yahoo.co.id>
2015-10-12 11:30:11 +0700

    Second Commit

--- modules/product/views/index.php
+++ modules/product/views/index.php
@@ -1,13 +1,12 @@
 <?php echo $this->session->flashdata('message'); ?>
 <a href="add" class="waves-effect waves-light btn">Add Product</a>
+<p class="flow-text">* Click "<b>Product</b>" for details</p>
 <table class="hoverable" id="table">
     <thead>
         <tr>
             <th>No.</th>
             <th>Product</th>
             <th>Price</th>
-            <th></th>
-            <th></th>
         </tr>
     </thead>
     <tbody>
@@ -17,13 +16,8 @@
 ?>
         <tr>
             <td><?php echo $no; ?>.</td>
-            <td><?php echo $product->product_name; ?></td>
+            <td><a class="modal-trigger" href="#<?php echo $product->product_id; ?>"><?php echo $product->product_name; ?></a></td>
             <td>Rp. <?php echo number_format($product->price,0,',','.'); ?></td>
-            <td><a class="modal-trigger" href="#<?php echo $product->product_id; ?>">Detail</a></td>
-            <td>
-                <a href="edit/<?php echo $product->product_id; ?>"><i class="material-icons">edit</i></a>
-                <a href="delete/<?php echo $product->product_id; ?>" onclick="return confirmDelete();"><i class="material-icons">delete</i></a>
-            </td>
         </tr>
 <?php
     $no ++;
@@ -32,16 +26,20 @@
     </tbody>
 </table>
 <?php
-    foreach($data->result_array() AS $detail) {
+    foreach($data->result() AS $product) {
 ?>
-<div id="<?php echo $detail['product_id']; ?>" class="modal">
-    <div class="modal-content">
-        <h5><?php echo $detail['product_name']; ?></h5>
+<div id="<?php echo $product->product_id; ?>" class="modal">
+    <div class="modal-content">        
         <p class="flow-text">
-            Category Product : <?php echo $detail['category_name']; ?><br>            
-            <?php echo $detail['description']; ?><br>
-            <a href="images/<?php echo $detail['product_id']; ?>" class="waves-effect waves-light btn">View Images</a>
+            <b><?php echo $product->product_name; ?></b><br>
+            Category Product : <?php echo $product->category_name; ?><br>
+            <?php echo $product->description; ?><br>            
         </p>
+        <div class="center">
+            <a href="images/<?php echo $product->product_id; ?>" class="waves-effect waves-light btn light-blue">View Images</a><br><br>
+            <a href="edit/<?php echo $product->product_id; ?>" class="waves-effect waves-light btn light-blue"><i class="material-icons">edit</i></a>
+            <a href="delete/<?php echo $product->product_id; ?>" class="waves-effect waves-light btn light-blue" onclick="return confirmDelete();"><i class="material-icons">delete</i></a>
+        </div>
     </div>
     <div class="modal-footer">
         <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Close</a>
